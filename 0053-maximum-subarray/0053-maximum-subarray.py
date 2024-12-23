5dd11b6d6a51ae936df29b5611ec6f7cc07b838d
Shaurya Negi <shauryanegi17@gmail.com>
2024-12-23 19:55:20 +0530

    0053-MAXIMUM-SUBARRAY Stats: Time: 59 ms (60.04%), Space: 32.7 MB (5.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,14 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        curSum = 0
-        maxSum = nums[0]
+        curSum = 0       # Initialize the current subarray sum to 0
+        maxSum = nums[0]  # Initialize the maximum sum to the first element of the array
 
-        for num in nums:
-            if curSum < 0:
-                curSum = 0
+        for num in nums:   # Iterate over each number in the input array
+            if curSum < 0: # if curSum is negative
+                curSum = 0  # Reset curSum as a negative curSum would only make any subsequent subarray's sum smaller
             
-            curSum += num
-            maxSum = max(curSum, maxSum)
-        return maxSum
-
-    
+            curSum += num  # Add the current number to the curSum
+            maxSum = max(curSum, maxSum) # Update maxSum if curSum is greater
+        return maxSum #return the max sum of any contiguous subarray
