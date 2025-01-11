fead53e9a078f90ef8ef2428d1ccd98eaa52bd8c
Shaurya Negi <shauryanegi17@gmail.com>
2025-01-11 23:21:14 +0530

    0021-MERGE-TWO-SORTED-LISTS Stats: Time: 0 ms (100%), Space: 17.9 MB (15.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -13,19 +13,17 @@ class Solution:
             if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
+
             else:
                 tail.next = list2
                 list2 = list2.next
-            
             tail = tail.next
 
         if list1:
             tail.next = list1
         elif list2:
             tail.next = list2
-
         return dummy.next
-
 
     # Time complexity: O(n+m)
     # Space complexity: O(1)
