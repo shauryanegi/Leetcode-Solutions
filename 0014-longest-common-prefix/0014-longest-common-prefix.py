c8bdcd5c4ebe4adabaa0ebe0c5c6cbf18f9d6dc9
Shaurya Negi <shauryanegi17@gmail.com>
2025-01-13 15:15:22 +0530

    0014-LONGEST-COMMON-PREFIX Stats: Time: 2 ms (33.88%), Space: 18 MB (13.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,8 +1,8 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        for i in range(len(strs[0])):
-            for s in strs:
+        for i in range(len(strs[0])): # Only first string
+            for s in strs: 
                 if i == len(s) or s[i] != strs[0][i]:
                     return s[:i]
 
