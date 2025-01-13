32de970cb0d8693433580e22a802515ca51c16f6
Shaurya Negi <shauryanegi17@gmail.com>
2025-01-13 17:57:56 +0530

    0014-LONGEST-COMMON-PREFIX Stats: Time: 0 ms (100%), Space: 18 MB (13.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,12 +1,20 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        for i in range(len(strs[0])): # Only first string
-            for s in strs: 
-                if i == len(s) or s[i] != strs[0][i]:
-                    return s[:i]
+        min_length = float("inf")
 
-        return strs[0]
+        for s in strs:
+            if len(s) < min_length:
+                min_length = len(s)
+
+        i = 0
+        while i < min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i += 1
+
+        return s[:i]
 
         # Time complexity: O(nâm)
         # Space complexity: O(1) since we did not use extra space
