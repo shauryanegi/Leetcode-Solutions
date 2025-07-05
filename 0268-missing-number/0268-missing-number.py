e69f6a4ff896954509a9c0cd96bd1fdaeae10957
Shaurya Negi <shauryanegi17@gmail.com>
2025-07-06 02:15:49 +0530

    Time: 2 ms (66.66%), Space: 18.7 MB (51.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,22 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
 
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
-        return expected_sum - sum(nums)
+        # n = len(nums)
+        # expected_sum = n * (n + 1) // 2
+        # return expected_sum - sum(nums)
+
+        # More Efficient way
+        
+        xor1 = 0
+        xor2 = 0
+
+        for i in range(len(nums)):
+            xor1 = xor1 ^ i
+            xor2 = xor2 ^ nums[i]
+
+        xor1 = xor1 ^ len(nums) # last element as it got left out in the loop
+
+        return xor1 ^ xor2
 
         # Time Complexity: O(n), Space Complexity: O(1)
 
